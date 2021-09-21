6763e4aefd528483d0c4eaa05367b23495393caf
skidfest <85274761+skidfest@users.noreply.github.com>
2021-09-21 13:39:30 +0530

    Update up.php

--- up.php
+++ up.php
@@ -1,7 +1,7 @@
 <?php
 $secret_key = "KEYGOESHERE"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "team/"; //This is your file dir, also the link..
-$domain_url = 'https://dax.skidfest.ga/'; //Add an S at the end of HTTP if you have a SSL certificate.
+$domain_url = 'https://skidfest.ga/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
 function RandomString($length) {
